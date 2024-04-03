b56ee03896370b08a4983fda1f2a82ecb1c9bd57
eman ehab <emanehab1273@gmail.com>
2024-04-03 02:31:23 +0200

    2nd update

--- tame_it/lib/Screens/navbar_Root_Screens/more pages/changePassword.dart
+++ tame_it/lib/Screens/navbar_Root_Screens/more pages/changePassword.dart
@@ -16,157 +16,149 @@ class _changePasswordState extends State<changePassword> {
 
   @override
   Widget build(BuildContext context) {
-    var heightOfScreen = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        body: ListView(children: <Widget>[
-                Container(
-                  height: heightOfScreen,
-                  child: _buildChangePassForm(),
-                )
-        ]));
+        body: Container(
+      child: _buildChangePassForm(),
+    ));
   }
 
   Widget _buildChangePassForm() {
     var widthOfScreen = MediaQuery.of(context).size.width;
     ThemeData theme = Theme.of(context);
     return Scaffold(
-      backgroundColor: AppColors.whiteShade3,
-      appBar: AppBar(
-          title: Center(
-              child: Text('Change Password',
+        backgroundColor: AppColors.whiteShade3,
+        appBar: AppBar(
+            title: Center(
+                child: Text('Change Password',
+                    style: TextStyle(
+                      color: AppColors.deepsea,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ))),
+            iconTheme: const IconThemeData(color: AppColors.deepsea),
+            backgroundColor: AppColors.whiteShade3),
+        body: Container(
+            margin: EdgeInsets.symmetric(horizontal: Sizes.MARGIN_16),
+            child: Column(children: <Widget>[
+              SizedBox(
+                height: Sizes.HEIGHT_14,
+              ),
+              SizedBox(
+                height: Sizes.HEIGHT_50,
+                child: TextField(
+                  keyboardType: TextInputType.visiblePassword,
                   style: TextStyle(
-                    color: AppColors.deepsea,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400,
-                  ))),
-          iconTheme: const IconThemeData(color: AppColors.deepsea),
-          backgroundColor: AppColors.whiteShade3),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: Sizes.MARGIN_16),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: Sizes.HEIGHT_14,
-            ),
-            SizedBox(
-              height: Sizes.HEIGHT_50,
-              child: TextField(
-                keyboardType: TextInputType.visiblePassword,
-                style: TextStyle(
-                  fontSize: Sizes.TEXT_SIZE_14,
+                    fontSize: Sizes.TEXT_SIZE_14,
+                  ),
+                  decoration: InputDecoration(
+                      // contentPadding:EdgeInsets.only(left: 25),
+                      hintText: ' Enter current password',
+                      hintStyle: Styles.customTextStyle(
+                        fontSize: Sizes.TEXT_SIZE_14,
+                        color: Color.fromARGB(255, 137, 182, 177),
+                      ),
+                      enabled: true,
+                      enabledBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(Sizes.RADIUS_14)),
+                          borderSide: BorderSide(
+                              color: AppColors.grey,
+                              width: Sizes.WIDTH_1,
+                              style: BorderStyle.solid)),
+                      filled: true,
+                      fillColor: Color.fromARGB(94, 200, 201, 205)),
                 ),
-                decoration: InputDecoration(
-                    // contentPadding:EdgeInsets.only(left: 25),
-                    hintText: ' Enter current password',
-                    hintStyle: Styles.customTextStyle(
-                      fontSize: Sizes.TEXT_SIZE_14,
-                      color: Color.fromARGB(255, 137, 182, 177),
-                    ),
-                    enabled: true,
-                    enabledBorder: const OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.all(Radius.circular(Sizes.RADIUS_14)),
-                        borderSide: BorderSide(
-                            color: AppColors.grey,
-                            width: Sizes.WIDTH_1,
-                            style: BorderStyle.solid)),
-                    filled: true,
-                    fillColor: Color.fromARGB(94, 200, 201, 205)),
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              height: Sizes.HEIGHT_50,
-              child: TextField(
-                keyboardType: TextInputType.visiblePassword,
-                style: TextStyle(
-                  fontSize: Sizes.TEXT_SIZE_14,
+              SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                height: Sizes.HEIGHT_50,
+                child: TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  style: TextStyle(
+                    fontSize: Sizes.TEXT_SIZE_14,
+                  ),
+                  decoration: InputDecoration(
+                      // contentPadding:EdgeInsets.only(left: 25),
+                      hintText: ' New password',
+                      hintStyle: Styles.customTextStyle(
+                        fontSize: Sizes.TEXT_SIZE_14,
+                        color: Color.fromARGB(255, 137, 182, 177),
+                      ),
+                      suffixIcon: const Icon(
+                        FeatherIcons.eyeOff,
+                        size: Sizes.ICON_SIZE_22,
+                        color: Color.fromARGB(255, 137, 182, 177),
+                      ),
+                      enabled: true,
+                      enabledBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(Sizes.RADIUS_14)),
+                          borderSide: BorderSide(
+                              color: AppColors.grey,
+                              width: Sizes.WIDTH_1,
+                              style: BorderStyle.solid)),
+                      filled: true,
+                      fillColor: Color.fromARGB(94, 200, 201, 205)),
+                  obscureText: true,
                 ),
-                decoration: InputDecoration(
-                    // contentPadding:EdgeInsets.only(left: 25),
-                    hintText: ' New password',
-                    hintStyle: Styles.customTextStyle(
-                      fontSize: Sizes.TEXT_SIZE_14,
-                      color: Color.fromARGB(255, 137, 182, 177),
-                    ),
-                    suffixIcon: const Icon(
-                      FeatherIcons.eyeOff,
-                      size: Sizes.ICON_SIZE_22,
-                      color: Color.fromARGB(255, 137, 182, 177),
-                    ),
-                    enabled: true,
-                    enabledBorder: const OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.all(Radius.circular(Sizes.RADIUS_14)),
-                        borderSide: BorderSide(
-                            color: AppColors.grey,
-                            width: Sizes.WIDTH_1,
-                            style: BorderStyle.solid)),
-                    filled: true,
-                    fillColor: Color.fromARGB(94, 200, 201, 205)),
-                obscureText: true,
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            SizedBox(
-              height: Sizes.HEIGHT_50,
-              child: TextField(
-                keyboardType: TextInputType.visiblePassword,
-                style: TextStyle(
-                  fontSize: Sizes.TEXT_SIZE_14,
+              SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                height: Sizes.HEIGHT_50,
+                child: TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  style: TextStyle(
+                    fontSize: Sizes.TEXT_SIZE_14,
+                  ),
+                  decoration: InputDecoration(
+                      // contentPadding:EdgeInsets.only(left: 25),
+                      hintText: ' Confirm new password',
+                      hintStyle: Styles.customTextStyle(
+                        fontSize: Sizes.TEXT_SIZE_14,
+                        color: Color.fromARGB(255, 137, 182, 177),
+                      ),
+                      suffixIcon: const Icon(
+                        FeatherIcons.eyeOff,
+                        size: Sizes.ICON_SIZE_22,
+                        color: Color.fromARGB(255, 137, 182, 177),
+                      ),
+                      enabled: true,
+                      enabledBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(Sizes.RADIUS_14)),
+                          borderSide: BorderSide(
+                              color: AppColors.grey,
+                              width: Sizes.WIDTH_1,
+                              style: BorderStyle.solid)),
+                      filled: true,
+                      fillColor: Color.fromARGB(94, 200, 201, 205)),
+                  obscureText: true,
                 ),
-                decoration: InputDecoration(
-                    // contentPadding:EdgeInsets.only(left: 25),
-                    hintText: ' Confirm new password',
-                    hintStyle: Styles.customTextStyle(
-                      fontSize: Sizes.TEXT_SIZE_14,
-                      color: Color.fromARGB(255, 137, 182, 177),
-                    ),
-                    suffixIcon: const Icon(
-                      FeatherIcons.eyeOff,
-                      size: Sizes.ICON_SIZE_22,
-                      color: Color.fromARGB(255, 137, 182, 177),
-                    ),
-                    enabled: true,
-                    enabledBorder: const OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.all(Radius.circular(Sizes.RADIUS_14)),
-                        borderSide: BorderSide(
-                            color: AppColors.grey,
-                            width: Sizes.WIDTH_1,
-                            style: BorderStyle.solid)),
-                    filled: true,
-                    fillColor: Color.fromARGB(94, 200, 201, 205)),
-                obscureText: true,
               ),
-            ),
-            SizedBox(
-              height: 35,
-            ),
-            Container(
-              width: widthOfScreen * 0.6,
-              child: CustomButton(
-                title: 'Save',
-                color: AppColors.deepsea,
-                textStyle: theme.textTheme.bodyMedium?.copyWith(
-                  color: AppColors.white,
-                  fontSize: Sizes.TEXT_SIZE_16,
-                  fontWeight: FontWeight.w600,
+              SizedBox(
+                height: 35,
+              ),
+              Container(
+                width: widthOfScreen * 0.6,
+                child: CustomButton(
+                  title: 'Save',
+                  color: AppColors.deepsea,
+                  textStyle: theme.textTheme.bodyMedium?.copyWith(
+                    color: AppColors.white,
+                    fontSize: Sizes.TEXT_SIZE_16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  onPressed: () {},
                 ),
-                onPressed: () {},
               ),
-            ),
-            SizedBox(
-              height: Sizes.HEIGHT_25,
-            ),
-          ],
-        ),
-      ),
-    );
+              SizedBox(
+                height: Sizes.HEIGHT_25,
+              )
+            ])));
   }
 }
