4d1542becf28458d334706bd17ddeb99c11da46c
Eman Ehab <emanehab1273@gmail.com>
2024-04-13 02:54:50 +0200

    Doctor Details

--- tame_it/lib/Screens/navbar_Root_Screens/Home.dart
+++ tame_it/lib/Screens/navbar_Root_Screens/Home.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tame_it/values/values.dart';
 
@@ -13,83 +15,76 @@ class __HomeStateState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.whiteShade3,
-      appBar: AppBar(
         backgroundColor: AppColors.whiteShade3,
-        shape: const Border(
-          bottom: BorderSide(
-            color: AppColors.whiteShade4,
-            width: 1,
-          ),
-        ),
-        title: const Text(
-          'Home Page',
-          style: TextStyle(
-            color: AppColors.deepsea,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/Profile');
-            },
-            icon: const CircleAvatar(
-              radius: 17,
-              backgroundColor: Colors.black38,
-              child: CircleAvatar(
-                radius: 16,
-                backgroundImage: AssetImage('assets/images/123.jpg'),
+        appBar: AppBar(
+            backgroundColor: AppColors.whiteShade3,
+            shape: const Border(
+              bottom: BorderSide(
+                color: AppColors.whiteShade4,
+                width: 1,
               ),
             ),
-          )
-        ],
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(15.0),
-                child: Text(
-                  'Welcome to TAME IT',
-                  style: GoogleFonts.jacquesFrancois(
-                    textStyle: const TextStyle(
-                      color: AppColors.deepsea,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
+            title: const Text(
+              'Home Page',
+              style: TextStyle(
+                color: AppColors.deepsea,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/Profile');
+                  },
+                  icon: const CircleAvatar(
+                      radius: 17,
+                      backgroundColor: Colors.black38,
+                      child: CircleAvatar(
+                        radius: 16,
+                        backgroundImage: AssetImage('assets/images/123.jpg'),
+                      )))
+            ]),
+        body: SafeArea(
+            child: SingleChildScrollView(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
               Padding(
-                padding: EdgeInsets.all(15.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset(
-                    'assets/images/First Imgae.jpg',
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  ),
-                ),
-              ),
+                  padding: EdgeInsets.all(15.0),
+                  child: Text('Welcome to TAME IT',
+                      style: GoogleFonts.jacquesFrancois(
+                          textStyle: const TextStyle(
+                        color: AppColors.deepsea,
+                        fontSize: 18,
+                      )))),
+              Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'assets/images/First Imgae.jpg',
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      ))),
               const SizedBox(height: 12),
               Padding(
-                padding: EdgeInsets.only(left: 15.0),
-                child: Text(
-                  'Our Services',
-                  style: TextStyle(
-                    color: AppColors.deepsea,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
+                  padding: EdgeInsets.only(left: 15.0),
+                  child: Text('Our Services',
+                      style: TextStyle(
+                        color: AppColors.deepsea,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ))),
               const SizedBox(height: 5),
-              _buildServiceCard(
-                image: 'assets/images/p1.jpg',
-                title: 'Therapists',
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/Therapists');
+                },
+                child: _buildServiceCard(
+                  image: 'assets/images/p1.jpg',
+                  title: 'Therapists',
+                ),
               ),
               const SizedBox(height: 5),
               _buildServiceCard(
@@ -102,9 +97,14 @@ class __HomeStateState extends State<Home> {
                 title: 'ChatBot',
               ),
               const SizedBox(height: 5),
-              _buildServiceCard(
-                image: 'assets/images/p4.jpg',
-                title: 'Online Sessions',
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/OnlineTherapists');
+                },
+                child: _buildServiceCard(
+                  image: 'assets/images/p4.jpg',
+                  title: 'Online Sessions',
+                ),
               ),
               const SizedBox(height: 5),
               _buildServiceCard(
@@ -113,11 +113,7 @@ class __HomeStateState extends State<Home> {
                 apparent: true,
               ),
               const SizedBox(height: 200),
-            ],
-          ),
-        ),
-      ),
-    );
+            ]))));
   }
 
   Widget _buildServiceCard({
@@ -130,70 +126,60 @@ class __HomeStateState extends State<Home> {
     final cardWidth = screenSize.width * 0.9;
 
     return Center(
-      child: Container(
-        width: cardWidth,
-        height: cardHeight,
-        margin: EdgeInsets.symmetric(vertical: 8),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-          // boxShadow: [
-          //   BoxShadow(
-          //     color: Colors.grey.withOpacity(0.5),
-          //     spreadRadius: 5,
-          //     blurRadius: 7,
-          //     offset: Offset(0, 3),
-          //   ),
-          //  ],
-        ),
-        child: Row(
-          children: [
-            Container(
-              width: cardWidth * 0.3,
-              height: cardHeight,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  bottomLeft: Radius.circular(15),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                // borderRadius: BorderRadius.only(
-                //   topLeft: Radius.circular(15),
-                //   bottomLeft: Radius.circular(15),
-
-                // ),
-                borderRadius: BorderRadius.circular(15),
-
-                child: Image.asset(
-                  image,
-                  fit: BoxFit.cover,
-                  height: cardHeight, // Set image height to match card height
-                ),
-              ),
+        child: Container(
+            width: cardWidth,
+            height: cardHeight,
+            margin: EdgeInsets.symmetric(vertical: 8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              // boxShadow: [
+              //   BoxShadow(
+              //     color: Colors.grey.withOpacity(0.5),
+              //     spreadRadius: 5,
+              //     blurRadius: 7,
+              //     offset: Offset(0, 3),
+              //   ),
+              //  ],
             ),
-            SizedBox(width: cardWidth * 0.05),
-            Center(
-              child: Text(
-                title,
-                style: TextStyle(
-                  fontSize: screenSize.width * 0.05,
-                  color: AppColors.OrangePeel,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+            child: Row(children: [
+              Container(
+                  width: cardWidth * 0.3,
+                  height: cardHeight,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        bottomLeft: Radius.circular(15),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        )
+                      ]),
+                  child: ClipRRect(
+                      // borderRadius: BorderRadius.only(
+                      //   topLeft: Radius.circular(15),
+                      //   bottomLeft: Radius.circular(15),
+
+                      // ),
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset(
+                        image,
+                        fit: BoxFit.cover,
+                        height:
+                            cardHeight, // Set image height to match card height
+                      ))),
+              SizedBox(width: cardWidth * 0.05),
+              Center(
+                  child: Text(title,
+                      style: TextStyle(
+                        fontSize: screenSize.width * 0.05,
+                        color: AppColors.OrangePeel,
+                        fontWeight: FontWeight.bold,
+                      )))
+            ])));
   }
 }
---
+++ tame_it/lib/Screens/navbar_Root_Screens/branch_Screens/doctor_details.dart
@@ -0,0 +1,312 @@
+import 'package:flutter/material.dart';
+import 'package:tame_it/values/values.dart';
+
+class DoctorDetails extends StatefulWidget {
+  const DoctorDetails({super.key});
+
+  @override
+  State<DoctorDetails> createState() => _DoctorDetailsState();
+}
+
+class _DoctorDetailsState extends State<DoctorDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppColors.whiteShade3,
+        appBar: AppBar(
+          title: Text('Therapist Profile',
+              style: TextStyle(
+                color: AppColors.deepsea,
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+              )),
+          centerTitle: true,
+          iconTheme: const IconThemeData(color: AppColors.deepsea),
+        ),
+        body: Stack(children: [
+          CustomScrollView(slivers: [
+            SliverPersistentHeader(
+              delegate: _MySliverAppBar(expandedHeight: 250.0),
+              floating: false,
+              pinned: true,
+            ),
+
+            //    SliverToBoxAdapter(
+            //   child: Container(
+            //     color: AppColors.Bermuda,
+            //     height: 20,
+            //     child: Column(
+            //       mainAxisAlignment: MainAxisAlignment.end,
+            //       children: <Widget>[
+            //         Container(
+            //           height: 20,
+            //           decoration: BoxDecoration(
+            //             color: Colors.white,
+            //               borderRadius: BorderRadius.only(
+            //                 topLeft: const Radius.circular(20.0),
+            //                 topRight: const Radius.circular(20.0),
+            //               ),
+            //           ),
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+            SliverList(
+                delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                    padding: EdgeInsets.only(top: 16.0, left: 16.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(20.0),
+                        topRight: const Radius.circular(20.0),
+                      ),
+                      // color: AppColors.Bermuda
+                    ),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(children: [
+                            Text(
+                              'Dr. Georgia Griffin',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0,
+                                color: AppColors.deepsea,
+                              ),
+                            ),
+                            Expanded(
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                  IconButton(
+                                      onPressed: () {
+                                        Navigator.of(context)
+                                            .pushNamed('/ChatDoctorPage');
+                                      },
+                                      icon: Icon(
+                                        Icons.chat,
+                                        color: AppColors.deepsea,
+                                      ))
+                                ]))
+                          ]),
+                          Text('Sr. Psychologist'),
+                          SizedBox(height: 25),
+                          Row(children: [
+                            Text('Patients'),
+                            SizedBox(width: 45),
+                            Text('Experience'),
+                            SizedBox(width: 45),
+                            Text('Rating'),
+                          ]),
+                          SizedBox(height: 15),
+                          Row(children: [
+                            Text('1.4K',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            SizedBox(width: 90),
+                            Text('5 yr',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            SizedBox(width: 70),
+                            Text('4.0',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                          ]),
+                          SizedBox(height: 10),
+                          Text('Specialties',
+                              style: TextStyle(
+                                  color: AppColors.deepsea,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20)),
+                          SizedBox(height: 7),
+                          Column(children: [
+                            Row(children: [
+                              Container(
+                                  padding: EdgeInsets.only(
+                                      left: 20, right: 20, top: 8, bottom: 8),
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(32)),
+                                    color: AppColors.greyShade5,
+                                  ),
+                                  child: Text("OCD",
+                                      style: TextStyle(
+                                        color: AppColors.deepsea,
+                                        fontWeight: FontWeight.bold,
+                                      ))),
+                              SizedBox(width: 10),
+                              Container(
+                                  padding: EdgeInsets.only(
+                                      left: 20, right: 20, top: 8, bottom: 8),
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(32)),
+                                    color: AppColors.greyShade5,
+                                  ),
+                                  child: Text("ADHD",
+                                      style: TextStyle(
+                                        color: AppColors.deepsea,
+                                        fontWeight: FontWeight.bold,
+                                      ))),
+                              SizedBox(width: 10),
+                              Container(
+                                  padding: EdgeInsets.only(
+                                      left: 20, right: 20, top: 8, bottom: 8),
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(32)),
+                                    color: AppColors.greyShade5,
+                                  ),
+                                  child: Text("PTSD",
+                                      style: TextStyle(
+                                        color: AppColors.deepsea,
+                                        fontWeight: FontWeight.bold,
+                                      ))),
+                            ]),
+                            SizedBox(height: 10),
+                            Row(children: [
+                              Container(
+                                  padding: EdgeInsets.only(
+                                      left: 20, right: 20, top: 8, bottom: 8),
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(32)),
+                                    color: AppColors.greyShade5,
+                                  ),
+                                  child: Text("Anxiety",
+                                      style: TextStyle(
+                                        color: AppColors.deepsea,
+                                        fontWeight: FontWeight.bold,
+                                      ))),
+                              SizedBox(width: 10),
+                              Container(
+                                  padding: EdgeInsets.only(
+                                      left: 20, right: 20, top: 8, bottom: 8),
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(32)),
+                                    color: AppColors.greyShade5,
+                                  ),
+                                  child: Text("Mood Disorders",
+                                      style: TextStyle(
+                                        color: AppColors.deepsea,
+                                        fontWeight: FontWeight.bold,
+                                      ))),
+                            ]),
+                            SizedBox(height: 10),
+                            Row(children: [
+                              Container(
+                                  padding: EdgeInsets.only(
+                                      left: 20, right: 20, top: 8, bottom: 8),
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(32)),
+                                    color: AppColors.greyShade5,
+                                  ),
+                                  child: Text("Stress Scale",
+                                      style: TextStyle(
+                                        color: AppColors.deepsea,
+                                        fontWeight: FontWeight.bold,
+                                      ))),
+                              SizedBox(width: 10),
+                              Container(
+                                  padding: EdgeInsets.only(
+                                      left: 20, right: 20, top: 8, bottom: 8),
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(32)),
+                                    color: AppColors.greyShade5,
+                                  ),
+                                  child: Text("Depression",
+                                      style: TextStyle(
+                                        color: AppColors.deepsea,
+                                        fontWeight: FontWeight.bold,
+                                      )))
+                            ])
+                          ]),
+                          SizedBox(height: 30),
+                          Text(
+                            'About',
+                            style: TextStyle(
+                                color: AppColors.deepsea,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                          SizedBox(height: 15),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16),
+                            child: Text(
+                                'I am a licensed psychotherapist from the Egyptian Ministry of Health and the Qatar Supreme Council of Health. \n\n'
+                                'I obtained my Phd in Psychology from Ain Shams University and have attended training courses, including cognitive-behavioural '
+                                'therapy (CBT) in depression and anxiety from Beck Institute, personality disorders, and obsessive-compulsive disorder (OCD).'
+                                ' I am also licensed and certified to diagnose and treat Autism Spectrum Disorder with ADI-R and ADOS 2016-2018. \n\n'
+                                'I am also the head of the Administration of Prep Middle East Academy for Psychological Services.',
+                                maxLines: null),
+                          ),
+                          SizedBox(height: 200)
+                        ]));
+              },
+              childCount: 1,
+            ))
+          ]),
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                  width: double.infinity,
+                  color: AppColors.whiteShade3,
+                  child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {},
+                                child: Text('Book Appointment',
+                                    style: TextStyle(color: AppColors.white)),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: AppColors.deepsea,
+                                ))
+                          ]))))
+        ]));
+  }
+}
+
+class _MySliverAppBar extends SliverPersistentHeaderDelegate {
+  final double expandedHeight;
+
+  _MySliverAppBar({required this.expandedHeight});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Stack(
+      clipBehavior: Clip.none,
+      fit: StackFit.expand,
+      children: [
+        Image.network(
+          'https://th.bing.com/th/id/R.5f72006c3d7e4348fca79cacb13f1f49?rik=SFq5KC3b6u52oA&pid=ImgRaw&r=0',
+          fit: BoxFit.cover,
+        ),
+        Positioned(
+            top: expandedHeight - shrinkOffset - kToolbarHeight,
+            left: 0,
+            right: 0,
+            child: Opacity(
+              opacity: (1 - shrinkOffset / expandedHeight),
+            ))
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => expandedHeight;
+
+  @override
+  double get minExtent => kToolbarHeight;
+
+  @override
+  bool shouldRebuild(_MySliverAppBar oldDelegate) {
+    return expandedHeight != oldDelegate.expandedHeight;
+  }
+}
