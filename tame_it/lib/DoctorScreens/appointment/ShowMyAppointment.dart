dc4b2563cd918dfe54b7e5cacdb1c299a8c842a9
Rokaya_fawzy <120643097+RokayaFawzy@users.noreply.github.com>
2024-06-16 09:49:45 +0300

    show appointment page (Doctor interface)

--- tame_it/lib/DoctorScreens/appointment/ShowMyAppointment.dart
+++ tame_it/lib/DoctorScreens/appointment/ShowMyAppointment.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tame_it/DoctorScreens/appointment/Your_Session.dart';
 
 import '../../values/values.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class UserDetails {
+  final String userName;
+  final String email;
+  final String? imageUrl;
+
+  UserDetails({
+    required this.userName,
+    required this.email,
+    this.imageUrl,
+  });
+
+  factory UserDetails.fromJson(Map<String, dynamic> json) {
+    return UserDetails(
+      userName: json['userName'] ?? '',
+      email: json['email'] ?? '',
+      imageUrl: json['imageUrl'],
+    );
+  }
+}
 
 class ShowMyAppointment extends StatefulWidget {
   const ShowMyAppointment({super.key});
@@ -12,6 +35,8 @@ class ShowMyAppointment extends StatefulWidget {
 }
 
 class _ShowMyAppointmentState extends State<ShowMyAppointment> {
+  late Future<UserDetails> userDetails;
+
   final TextEditingController _dateController = TextEditingController();
   final List<String> _sessionTimes = [
     "10:00 AM",
@@ -19,6 +44,40 @@ class _ShowMyAppointmentState extends State<ShowMyAppointment> {
     "3:00 PM",
     "5:00 PM"
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    userDetails = fetchUserDetails();
+  }
+
+  Future<UserDetails> fetchUserDetails() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    if (token == null) {
+      throw Exception('Token not found');
+    }
+
+    try {
+      final response = await http.get(
+        Uri.parse('https://tameit.azurewebsites.net/api/auth/userDetails'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        return UserDetails.fromJson(responseData);
+      } else {
+        throw Exception(
+            'Failed to load user details. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching user details: $e');
+    }
+  }
 
   void _pickDate() async {
     DateTime? pickedDate = await showDatePicker(
@@ -45,6 +104,69 @@ class _ShowMyAppointmentState extends State<ShowMyAppointment> {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<UserDetails>(
+      future: userDetails,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          if (snapshot.data != null) {
+            final imageProvider = snapshot.data!.imageUrl != null
+                ? NetworkImage(snapshot.data!.imageUrl!)
+                : const AssetImage('assets/images/newlogo.jpg');
+            return buildUI(
+                snapshot.data!, imageProvider as ImageProvider<Object>);
+          } else {
+            return Text('User details not available');
+          }
+        }
+      },
+    );
+  }
+
+  Widget _buildClinicField(int index) {
+    return Column(
+      children: [
+        TextFormField(
+          decoration: InputDecoration(
+            hintText: 'Clinic Name',
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.deepsea),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.orange),
+            ),
+          ),
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+            hintText: 'Address',
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.deepsea),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.orange),
+            ),
+          ),
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+            hintText: 'Phone Number',
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.deepsea),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.orange),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildUI(UserDetails userDetails, ImageProvider imageProvider) {
     return Scaffold(
       backgroundColor: AppColors.white,
       appBar: AppBar(
@@ -71,7 +193,7 @@ class _ShowMyAppointmentState extends State<ShowMyAppointment> {
                   Padding(
                     padding: EdgeInsets.all(15.0),
                     child: Text(
-                      'Hi  ' + ' Dr. Sara',
+                      'Hi Dr. ' + userDetails.userName,
                       style: TextStyle(
                           fontSize: 23,
                           color: AppColors.OrangePeel,
@@ -126,12 +248,61 @@ class _ShowMyAppointmentState extends State<ShowMyAppointment> {
                       ],
                     ),
                   ),
-                  SizedBox(height: 25),
+                  SizedBox(height: 15),
                   Padding(
                     padding: EdgeInsets.only(left: 15.0, right: 30.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Row(
+                            children: [
+                              Text(
+                                "Fees",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColors.deepsea,
+                                  fontFamily: "Domine",
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Fees Is ',
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColors.deepsea),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.orange),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Row(
+                            children: [
+                              Text(
+                                "Clinic",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColors.deepsea,
+                                  fontFamily: "Domine",
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: List.generate(
+                              1, (clinicIndex) => _buildClinicField(2)),
+                        ),
+                        SizedBox(height: 25),
                         Text(
                           'You Have Session At',
                           style: TextStyle(
@@ -141,7 +312,7 @@ class _ShowMyAppointmentState extends State<ShowMyAppointment> {
                             fontWeight: FontWeight.w700,
                           ),
                         ),
-                        SizedBox(height: 13),
+                        SizedBox(height: 10),
                         Wrap(
                           spacing: 2.0,
                           runSpacing: 2.0,
